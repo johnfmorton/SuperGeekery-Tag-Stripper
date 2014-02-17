09ed0ab95074d5a31882bf5ae6cdab65949e7139
John Morton <john@johnfmorton.com>
2014-02-17 12:13:05 -0500

    Added support for removing non-breaking spaces

--- system/expressionengine/third_party/tagstripper/config.php
+++ system/expressionengine/third_party/tagstripper/config.php
@@ -4,12 +4,12 @@
 * SuperGeekery TagStripper Config file
 *
 * @package			tagstripper-addon
-* @version			1.0.2
+* @version			1.0.3
 * @author			John F Morton <john@johnfmorton.com>
 * @link				http://supergeekery.com/geekblog/comments/expression_engine_2_plugin_supergeekery_tag_stripper_version_1.0
 * @license			http://creativecommons.org/licenses/by-sa/3.0/
 */
 
 $config['name']='SuperGeekery Tag Stripper';
-$config['version']='1.0.2';
+$config['version']='1.0.3';
 $config['nsm_addon_updater']['versions_xml']='http://static.supergeekery.com/changelog_sgtagstripper.xml';
