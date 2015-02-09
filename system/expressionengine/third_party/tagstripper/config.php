5120c2155ca967228f9629eb6352fdc39913a234
John Morton <john@johnfmorton.com>
2015-02-09 13:06:57 -0500

    Updated to reflect 1.0.6 version numbering
    Updated functions to "public static function" to prevent PHP warnings in main pi.tagstripper.php.

--- system/expressionengine/third_party/tagstripper/config.php
+++ system/expressionengine/third_party/tagstripper/config.php
@@ -4,7 +4,7 @@
 * SuperGeekery TagStripper Config file
 *
 * @package			tagstripper-addon
-* @version			1.0.4
+* @version			1.0.6
 * @author			John F Morton <john@johnfmorton.com>
 * @link				http://supergeekery.com/geekblog/comments/expression_engine_2_plugin_supergeekery_tag_stripper_version_1.0
 * @license			http://creativecommons.org/licenses/by-sa/3.0/
@@ -13,7 +13,7 @@
 if ( ! defined('TAG_STRIPPER_NAME')) {
 	define('TAG_STRIPPER_NAME', 'SuperGeekery Tag Stripper');
 	define('TAG_STRIPPER_PACKAGE', 'tagstripper');
-	define('TAG_STRIPPER_VERSION', '1.0.5');
+	define('TAG_STRIPPER_VERSION', '1.0.6');
 	define('TAG_STRIPPER_DOCS', 'https://github.com/johnfmorton/SuperGeekery-Tag-Stripper');
 }
 
