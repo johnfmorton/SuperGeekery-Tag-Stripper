8afc68d9346d5ff0e9a7e4f1300904fe954f6e93
Andrew Reynolds <areynold@illinois.edu>
2019-05-23 15:15:12 -0500

    stub addon.setup.php

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php if (!defined('BASEPATH')) { exit ('No direct script access allowed.'); };
+
+return array(
+    'author' => '',
+    'author_url' => '',
+    'name' => '',
+    'description' => '',
+    'version' => '',
+    'namespace' => ''
+);
