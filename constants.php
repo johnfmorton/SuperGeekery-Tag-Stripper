99ad9e902c5c2ba01fb1df6955e4f1dd1994f2a4
Andrew Reynolds <areynold@illinois.edu>
2019-05-23 16:54:22 -0500

    capture config comments for posterity

--- constants.php
+++ constants.php
@@ -6,6 +6,15 @@ if (!defined('BASEPATH')) {
     exit ('No direct script access allowed.');
 }
 
+/**
+* SuperGeekery TagStripper Config file
+*
+* @package			tagstripper-addon
+* @version			1.1.0-alpha
+* @author			John F Morton <john@johnfmorton.com>
+* @link				http://supergeekery.com/geekblog/comments/expression_engine_2_plugin_supergeekery_tag_stripper_version_1.0
+* @license			http://creativecommons.org/licenses/by-sa/3.0/
+*/
 class Constants {
     const NAME = 'SuperGeekery Tag Stripper';
 
