fff660b49c82b5336b4ea90b734364ff3e391eec
Andrew Reynolds <areynold@illinois.edu>
2019-05-23 16:47:43 -0500

    correct class constant declaration

--- constants.php
+++ constants.php
@@ -7,15 +7,15 @@ if (!defined('BASEPATH')) {
 }
 
 class Constants {
-    public const NAME = 'SuperGeekery Tag Stripper';
+    const NAME = 'SuperGeekery Tag Stripper';
 
-    public const AUTHOR = 'John Morton';
+    const AUTHOR = 'John Morton';
 
-    public const AUTHOR_URL = 'https://supergeekery.com/';
+    const AUTHOR_URL = 'https://supergeekery.com/';
 
-    public const DESCRIPTION = 'Strips HTML tags, en masse, selectively, or by exception.';
+    const DESCRIPTION = 'Strips HTML tags, en masse, selectively, or by exception.';
 
-    public const DOCS_URL = 'https://github.com/johnfmorton/SuperGeekery-Tag-Stripper';
+    const DOCS_URL = 'https://github.com/johnfmorton/SuperGeekery-Tag-Stripper';
     
-    public const VERSION = '2.0.0-alpha';
+    const VERSION = '2.0.0-alpha';
 }
